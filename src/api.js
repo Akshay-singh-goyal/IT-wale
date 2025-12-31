c34f72d76d887b0a55116021f31e7633dc39bd55
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2025-12-31 15:46:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sm-backend-8me3.onrender.com", // backend URL
+  baseURL: "https://sm-backend-8me3.onrender.com/api", // add /api here
 });
 
 api.interceptors.request.use((config) => {
