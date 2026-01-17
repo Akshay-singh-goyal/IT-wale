ec1a1e62c5d50360b16bc04de5e61ec633ad4bcc
Akshay Singh Goyal <akshaysinghrajput702@gmail.com>
2026-01-17 15:07:12 +0530

    frist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 
+/* ===== COMPONENTS ===== */
 import Navbar from "./components/Home/Navbar";
 import Footer from "./components/Footer";
 
+/* ===== PAGES ===== */
 import Home from "./components/Home/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -13,7 +15,7 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import UserNotes from "./components/UserNotes";
 
-
+/* ===== PUBLIC LAYOUT ===== */
 const PublicLayout = () => (
   <>
     <Navbar />
@@ -22,10 +24,12 @@ const PublicLayout = () => (
   </>
 );
 
+/* ===== APP ===== */
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes with layout */}
         <Route element={<PublicLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
@@ -35,12 +39,10 @@ export default function App() {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="user-notes" element={<UserNotes />} />
-
-
         </Route>
 
-        {/* TEMPORARILY REMOVE THIS */}
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        {/* Optional: redirect unknown routes */}
+        {/* <Route path="*" element={<Home />} /> */}
       </Routes>
     </Router>
   );
